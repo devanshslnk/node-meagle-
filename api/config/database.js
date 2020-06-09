57dddf2563886f652507257d0a80c81dea2b5418
amit-99 <amitbhjbl99@gmail.com>
2020-06-10 01:21:49 +0530

    styling done 1

--- api/config/database.js
+++ api/config/database.js
@@ -6,8 +6,8 @@ dotenv.config()
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
-const username=process.env.DB_USERNAME
-const password=process.env.DB_PASSWORD
+const username="devanshslnk"
+const password="mongo"
 mongoose.connect(`mongodb+srv://${username}:${password}@cluster0-owkpo.mongodb.net/memeshare?retryWrites=true&w=majority`,{useNewUrlParser:true}).then(()=>{
    console.log("db connected");
 
